29c8a59d3cc30aef4f3b769bd7f554b9263d945b
Ardi002 <noviantoardi002@gmail.com>
2023-04-14 23:46:59 +0700

    new file:   dist/img/foto1.jpg 	new file:   dist/img/foto2.jpg 	new file:   dist/img/foto3.jpg 	new file:   dist/img/foto4.JPG 	modified:   dist/js/script.js 	modified:   index.html 	modified:   src/input.css

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,83 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+//function perulangan data
+function getHobbies() {
+  var hobbiesCount = parseInt(document.getElementById("hobbies").value);
+  if (isNaN(hobbiesCount) || hobbiesCount < 1) {
+    alert("Jumlah Pilihan Hobi harus lebih dari 0");
+    return;
+  }
+  var hobbiesInputs = "";
+  for (var i = 0; i < hobbiesCount; i++) {
+    hobbiesInputs +=
+      '<div class="group mt-6"><label for="hobby-' +
+      i +
+      '">Pilihan Hobi ' +
+      (i + 1) +
+      ':</label><input type="text" class="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary" id="hobby-' +
+      i +
+      '" required></div>';
+  }
+  document.getElementById("hobbies-inputs").innerHTML = hobbiesInputs;
+  document.getElementById("hobbies-list").style.display = "block";
+}
+
+function showHobbies() {
+  var hobbies = [];
+  var hobbyInputs = document
+    .getElementById("hobbies-inputs")
+    .getElementsByTagName("input");
+  for (var i = 0; i < hobbyInputs.length; i++) {
+    hobbies.push(hobbyInputs[i].value);
+  }
+  var hobbyOptions = "";
+  for (var i = 0; i < hobbies.length; i++) {
+    hobbyOptions +=
+      '<div class="form-check"><input class="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"' +
+      i +
+      '" value="' +
+      hobbies[i] +
+      '"><label class="form-check-label"' +
+      i +
+      '">' +
+      hobbies[i] +
+      "</label></div>";
+  }
+  document.getElementById("hobby-options").innerHTML = hobbyOptions;
+  document.getElementById("hobbies-select").style.display = "block";
+}
+
+function displayInfo() {
+  var firstName = document.getElementById("first-name").value;
+  var lastName = document.getElementById("last-name").value;
+  var email = document.getElementById("email").value;
+  var selectedHobbies = [];
+  var hobbyOptions = document
+    .getElementById("hobby-options")
+    .getElementsByTagName("input");
+  for (var i = 0; i < hobbyOptions.length; i++) {
+    if (hobbyOptions[i].checked) {
+      selectedHobbies.push(hobbyOptions[i].value);
+    }
+  }
+  if (selectedHobbies.length === 0) {
+    alert("Pilih setidaknya satu hobi");
+    return;
+  }
+  var message =
+    "Hallo, nama saya " +
+    firstName +
+    " " +
+    lastName +
+    ", dengan email " +
+    email +
+    ", saya mempunyai sejumlah " +
+    selectedHobbies.length +
+    " pilihan hobi yaitu " +
+    selectedHobbies.join(", ") +
+    ", dan saya menyukai " +
+    selectedHobbies.join(", ");
+  alert(message);
+}
